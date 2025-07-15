757681d789c9f2c5ff2a4b141490dee94eed8691
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-07-15 14:01:15 +0000

    [IMP] mail: search activities with access using SQL
    When possible, search for activities using condition expressed using a
    Domain.

--- addons/mail/models/mail_activity.py
+++ addons/mail/models/mail_activity.py
@@ -15,7 +15,7 @@ from odoo.tools.misc import clean_context, get_lang, groupby
 from odoo.tools.translate import LazyTranslate
 from odoo.addons.base.models.ir_attachment import condition_values
 from odoo.addons.mail.tools.discuss import Store
-from .mail_message import _find_allowed_doc_ids
+from .mail_message import _find_allowed_doc_ids, exists_in_cache
 
 _logger = logging.getLogger(__name__)
 _lt = LazyTranslate(__name__)
@@ -389,17 +389,67 @@ class MailActivity(models.Model):
             logic is detailed in :meth:`~._check_access`.
             Superusers bypass this and perform a standard search.
         """
+        domain = Domain(domain).optimize(self)
 
         if any(
             condition.field_expr == 'date_done' and condition.value
-            for condition in Domain(domain).iter_conditions()
+            for condition in domain.iter_conditions()
         ):
             kwargs['active_test'] = False
 
         # Rules do not apply to administrator or when we search only activities assigned to the current user
         domain = Domain(domain).optimize(self)
-        if self.env.su or bypass_access or tuple(condition_values(self, 'user_id', domain) or ()) == (self.env.uid,):
+        if self.env.su or bypass_access or domain.is_false() or tuple(condition_values(self, 'user_id', domain) or ()) == (self.env.uid,):
             return super()._search(domain, offset, limit, order, bypass_access=bypass_access, **kwargs)
+
+        # search by ids
+        if ids := condition_values(self, 'id', domain):
+            if (not order or order[:2] == 'id') and domain.map_conditions(lambda d: Domain.TRUE if d.field_expr == 'id' and d.operator == 'in' else d).is_true():
+                # trivial domain, can skip search, in most cases check access removes inexisting records
+                records = exists_in_cache(self.browse(ids), 'res_model')
+                records = records._filtered_access('read')
+                if order:
+                    records = records.sorted(order)
+            else:
+                records = self.browse(super()._search(domain, order=order, **kwargs))
+                records = records._filtered_access('read')
+            if offset > 0:
+                records = records[offset:]
+            if limit is not None:
+                records = records[:limit]
+            return records._as_query(ordered=bool(order))
+
+        # restrained to a few models
+        res_model_names = condition_values(self, 'res_model', domain) or ()
+        if 0 < len(res_model_names) <= 5:
+            # search by model and res_id
+            sec_domain = Domain('user_id', '=', self.env.uid)
+            env = self.with_context(active_test=False).env
+            for res_model_name in res_model_names:
+                if res_model_name not in env:
+                    continue
+                comodel = env[res_model_name]
+                codomain = Domain('res_model', '=', comodel._name)
+                comodel_res_ids = condition_values(self, 'res_id', domain.map_conditions(
+                    lambda cond: codomain & cond if cond.field_expr == 'res_model' else cond
+                ))
+                comodel_domain = Domain('id', 'in', comodel_res_ids) if comodel_res_ids else Domain.TRUE
+                operation = getattr(comodel, '_mail_post_access', 'read')
+                if operation == 'read':
+                    query = comodel._search(comodel_domain)
+                elif comodel.has_access(operation):
+                    comodel = comodel.sudo()
+                    comodel_domain &= self.env['ir.rule']._compute_domain(comodel._name, operation)
+                    query = comodel._search(comodel_domain)
+                else:
+                    continue
+                if query.is_empty():
+                    continue
+                if query.where_clause:
+                    codomain &= Domain('res_id', 'any!', query)
+                sec_domain |= codomain
+
+            return super()._search(domain & sec_domain, offset, limit, order, **kwargs)
 
         # retrieve activities and filter using their security rules
         query = super()._search(domain, offset, limit, order, **kwargs)

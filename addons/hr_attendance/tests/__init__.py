93658a936945f9cc84997d03cf40d36b18827794
hamzairfan <hamzairfanandroid@gmail.com>
2025-09-10 13:34:18 +0000

    [FIX] hr_attendance: prevent a user from updating their attendance record by changing the employee to the one whose attendance is not managed by the current user.
    Steps to reproduce:
    - Assign the Officer Group of Attendance group to a user.
    - Assign the user as the attendance manager of itself.
    - Login with that user.
    - Create an attendance record for the employee and save it.
    - Try to change the employee and save; an error will be thrown as expected.
    - Go to the Attendance menu; the record will still be saved.
    
    This commit ensures that unallowed write does not take place + test coverage added.
    Closes odoo/odoo#226007.
    
    X-original-commit: 9920eb67fb02d3039c84c6d78a0ca1c7c52f605a

--- addons/hr_attendance/tests/__init__.py
+++ addons/hr_attendance/tests/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from . import test_hr_attendance_constraints
 from . import test_hr_attendance_overtime
 from . import test_hr_attendance_undertime
@@ -7,5 +5,10 @@ from . import test_hr_attendance_process
 from . import test_hr_attendance_domain_translation
 from . import test_load_scenario
 from . import test_hr_attendance_kiosk
+<<<<<<< bda8d9778dd6e074c33a2e50d1b43fa85945f012
 from . import test_hr_attendance_rulesets
 from . import test_performance
+||||||| dc9dc90dd99f1f7dfb4a781ad2bec7378548d95b
+=======
+from . import test_hr_attendance_manager
+>>>>>>> 1baec8d1dff751d6e8d6a4b18f9a52223e9457bc

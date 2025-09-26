0a1928ae58bd0573fa8df9490509da0eba56ba4d
msho-odoo <msho@odoo.com>
2025-09-26 17:32:26 +0200

    [IMP] *: modify weight and volume in packages
    *: stock, stock_picking_batch, stock_delivery
    
    This commit improves the computation of weight and volume in packages.
    
    This commit:
    1- refactors the code by moving the logic of a package's weight
    and volume computation into `stock` module instead of `stock_delivery`.
    
    2- fixes the computation of the picking weight to include
    the base_weight of the package_type for all layers of the package
    in the picking and in the batch transfers.
    
    3- adds a computed `volume` field on the package. It is calculated using
    the package_type dimensions if exist, otherwise it uses the quant_ids.
    
    4- modifies the picking volume computation to take into account the
    `shipping_volume` of the package if exists (instead of the products),
    otherwise it computes the volume of what's inside the package, whether
    it includes other packages or products.
    
    Related upgrade PR: https://github.com/odoo/upgrade/pull/8749
    
    Task-5056474

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -196,6 +196,7 @@ class StockMove(models.Model):
     show_quant = fields.Boolean("Show Quant", compute="_compute_show_info")
     show_lots_m2o = fields.Boolean("Show lot_id", compute="_compute_show_info")
     show_lots_text = fields.Boolean("Show lot_name", compute="_compute_show_info")
+    weight = fields.Float(compute='_compute_move_weight', digits='Stock Weight', store=True, compute_sudo=True)
 
     _product_location_index = models.Index("(product_id, location_id, location_dest_id, company_id, state)")
 
@@ -730,6 +731,11 @@ Please change the quantity done or the rounding precision in your settings.""",
                 move.description_picking = product._get_picking_description(move.picking_type_id) or move._get_description()
             else:
                 move.description_picking = ""
+
+    @api.depends('product_id', 'product_uom_qty', 'product_uom', 'product_id.weight')
+    def _compute_move_weight(self):
+        for move in self:
+            move.weight = (move.product_qty * move.product_id.weight)
 
     def _get_description(self):
         product = self.product_id.with_context(lang=self._get_lang())

dd942833ec6381c73a2ec11c959cf4d62eaea5d0
Sébastien (blse) <blse@odoo.com>
2025-09-29 08:48:59 +0200

    wip use has_unsaved-data instead of history no undo

--- addons/website/static/src/builder/website_builder.js
+++ addons/website/static/src/builder/website_builder.js
@@ -54,7 +54,7 @@ export class WebsiteBuilder extends Component {
 
     async discard() {
         await revertPreview(this.editor);
-        if (this.editor.shared.history.canUndo()) {
+        if (this.editor.shared.savePlugin.hasUnsaveData()) {
             this.dialog.add(ConfirmationDialog, {
                 title: _t("Discard all changes?"),
                 body: _t(
@@ -75,7 +75,7 @@ export class WebsiteBuilder extends Component {
         if (!this.editor) {
             return;
         }
-        if (this.editor.shared.history.canUndo()) {
+        if (this.editor.shared.savePlugin.hasUnsaveData()) {
             event.preventDefault();
             event.returnValue = "Unsaved changes";
         }
@@ -85,7 +85,7 @@ export class WebsiteBuilder extends Component {
         if (!this.editor) {
             return true;
         }
-        if (this.editor.shared.history.canUndo()) {
+        if (this.editor.shared.savePlugin.hasUnsaveData()) {
             let continueProcess = true;
             await new Promise((resolve) => {
                 this.dialog.add(ConfirmationDialog, {

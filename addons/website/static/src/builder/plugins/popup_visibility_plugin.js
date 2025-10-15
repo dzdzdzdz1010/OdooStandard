e21dfde0c8eda9756af55aa884fec3545dbd49fb
Sébastien (blse) <blse@odoo.com>
2025-10-15 15:30:41 +0200

    [FIX] website: clean popup on save snippet
    There was a special handling for `s_popup` snippet when saving snippets:
    if the snippet was popup, it would not be cleaned directly, but instead
    its child would be cleaned. The goal was to avoid saving a hidden popup
    (like it normally is when saving a page), because it would show as an
    empty custom snippet.
    
    This commit instead do not close the popup if it is the root of what
    is saved. And do not make special cases inside the snippet service.
    
    Steps to reproduce:
    - Open website builder
    - Drop a popup
    - Click on the eye to hide the popup
    - Click on the eye to show the popup
    - Save the snippet as a custom snippet
    - Click to add a custom snippet
    - Bug: with the inspector, you can see that the `o_draggable` class has
      not been cleaned from the popup
    
    task-5149984

--- addons/website/static/src/builder/plugins/popup_visibility_plugin.js
+++ addons/website/static/src/builder/plugins/popup_visibility_plugin.js
@@ -96,7 +96,7 @@ export class PopupVisibilityPlugin extends Plugin {
     }
 
     cleanForSave({ root: rootEl }) {
-        for (const modalEl of rootEl.querySelectorAll(".s_popup .modal.show")) {
+        for (const modalEl of rootEl.querySelectorAll(".s_popup:not(:scope) > .modal.show")) {
             // Do not call .hide() directly, because it is queued whereas
             // .dispose() is not.
             modalEl.classList.remove("show");

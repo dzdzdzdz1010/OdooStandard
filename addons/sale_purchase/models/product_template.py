dcae1c9e2fd4f8fdf7fa386a6cb19ff021dfca1d
hapt-odoo <hapt@odoo.com>
2025-11-14 11:35:17 +0530

    [IMP] sale_purchase: move subcontract service in create on order
    - Previously, the Subcontract service option was available under the Purchase
      tab, making it harder to find.
    - This option is now added to the Create on Order selection field in the General
      tab, alongside other similar actions.
    - The service_tracking field has been made company-dependent to align with the
      existing behavior of the service_to_purchase field and ensure consistent
      functionality across companies.
    - This change improves usability by allowing users to easily find and configure
      subcontract service behavior for service-type products.
    - The related test cases have been updated to align with the new logic.
    
    Task ID: 5249083

--- addons/sale_purchase/models/product_template.py
+++ addons/sale_purchase/models/product_template.py
@@ -8,30 +8,40 @@ from odoo.exceptions import ValidationError
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    service_to_purchase = fields.Boolean(
-        "Subcontract Service", company_dependent=True, copy=False,
-        help="If ticked, each time you sell this product through a SO, a RfQ is automatically created to buy the product. Tip: don't forget to set a vendor on the product.")
+    service_tracking = fields.Selection(selection_add=[
+        ('subcontract', 'Subcontract Service')
+    ], ondelete={'subcontract': 'set default'})
 
-    @api.constrains('service_to_purchase', 'seller_ids', 'type')
-    def _check_service_to_purchase(self):
+    @api.constrains('service_tracking', 'seller_ids', 'type')
+    def _check_service_tracking_subcontract(self):
         for template in self:
-            if template.service_to_purchase:
+            if template.service_tracking == 'subcontract':
                 if template.type != 'service':
                     raise ValidationError(_("Product that is not a service can not create RFQ."))
-                template._check_vendor_for_service_to_purchase(template.seller_ids)
+                template._check_vendor_for_service_tracking_subcontract(template.seller_ids)
 
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
-            if vals.get('service_to_purchase'):
-                self._check_vendor_for_service_to_purchase(vals.get('seller_ids'))
+            if vals.get('service_tracking') == 'subcontract':
+                self._check_vendor_for_service_tracking_subcontract(vals.get('seller_ids'))
         return super().create(vals_list)
 
-    def _check_vendor_for_service_to_purchase(self, sellers):
+    def _check_vendor_for_service_tracking_subcontract(self, sellers):
         if not sellers:
             raise ValidationError(_("Please define the vendor from whom you would like to purchase this service automatically."))
 
     @api.onchange('type', 'expense_policy')
-    def _onchange_service_to_purchase(self):
+    def _onchange_service_tracking_subcontract(self):
         products_template = self.filtered(lambda p: p.type != 'service' or p.expense_policy != 'no')
-        products_template.service_to_purchase = False
+        products_template.service_tracking = 'no'
+
+    @api.depends('service_tracking')
+    def _compute_purchase_ok(self):
+        for template in self:
+            if template.service_tracking == 'subcontract' and not template.purchase_ok:
+                template.purchase_ok = True
+
+    @api.model
+    def _get_saleable_tracking_types(self):
+        return super()._get_saleable_tracking_types() + ['subcontract']

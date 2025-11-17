6926c828a8c9c5d08e29e165b097867c220a9aa4
Louis (loti) <loti@odoo.com>
2025-11-17 15:31:45 +0100

    [IMP] website_sale: clean up attribute value query params
    On the shop page, the attribute values were encoded as follows:
    ```
    attribute_values={attribute_id}-{attribute_value_1_id},{attribute_value_2_id}
    ```
    On the product page, the attribute values were encoded as follows:
    ```
    attribute_values={attribute_value_1_id},{attribute_value_2_id}
    ```
    The attribute values were not only encoded differently (which is confusing), but
    also unreadable (since we were showing integer ids).
    
    Now, the attribute values are encoded as follows, both on the shop and product
    pages:
    ```
    {attribute_slug}={attribute_value_1_slug},{attribute_value_2_slug}
    ```
    This addresses both issues pointed out above.
    
    task-5129517

--- addons/website_sale/models/product_template.py
+++ addons/website_sale/models/product_template.py
@@ -346,6 +346,7 @@ class ProductTemplate(models.Model):
         """
         res = defaultdict(dict)
         show_count = 20
+        slug = self.env['ir.http']._slug
         for template in self:
             previewed_ptal = next((
                 p for p in template.attribute_line_ids
@@ -364,7 +365,7 @@ class ProductTemplate(models.Model):
                         matching_variant = min(ptav.ptav_product_variant_ids, key=lambda p: p.id)
                         variant_query_params = {
                             **(product_query_params or {}),
-                            'attribute_values': str(ptav.product_attribute_value_id.id)
+                            slug(ptav.attribute_id): slug(ptav.product_attribute_value_id),
                         }
                         previewed_ptavs_data.append({
                             'ptav': ptav,
@@ -862,6 +863,7 @@ class ProductTemplate(models.Model):
         if category:
             domains.append([('public_categ_ids', 'child_of', self.env['ir.http']._unslug(category)[1])])
         if tags:
+            # TODO(loti): ensure that these are ints
             if isinstance(tags, str):
                 tags = tags.split(',')
             tags = list(map(int, tags))  # Convert list of strings to list of integers
@@ -1098,15 +1100,13 @@ class ProductTemplate(models.Model):
         url = urlparse(self.website_url)
         query_params = query_params or {}
         if grouped_attributes_values:
-            product_grouped_values = self.attribute_line_ids.value_ids.grouped('attribute_id')
-            available_pav_ids = [
-                next(v.id for v in pavs if v in product_grouped_values[pa])
-                for pa, pavs in grouped_attributes_values.items()
-                if pa in product_grouped_values
+            available_grouped_pavs = self.attribute_line_ids.value_ids.grouped('attribute_id')
+            pavs = [
+                next(pav for pav in pavs if pav in available_grouped_pavs[pa])
+                for pa, pavs in grouped_attributes_values.items() if pa in available_grouped_pavs
             ]
-            available_pav_ids.sort()
-            query_params['attribute_values'] = ','.join(str(i) for i in available_pav_ids)
-
+            slug = self.env['ir.http']._slug
+            query_params.update({slug(pav.attribute_id): slug(pav) for pav in pavs})
         if query_params:
             url = url._replace(query=urlencode(query_params, doseq=True))
 

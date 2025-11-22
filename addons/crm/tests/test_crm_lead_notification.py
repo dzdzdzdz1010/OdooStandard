2c5eac67ef65c857fe48170c230d45e25dfa9595
Renaud Thiry <reth@odoo.com>
2025-11-22 15:58:27 +0100

    [IMP] mail,*: remove MailThreadCC
    The main feature of MailThreadCC is now redundant with the ability for threads
    to suggest recipients from incoming email cc when relevant.
    
    It is completely removed.
    
    task-5013894

--- addons/crm/tests/test_crm_lead_notification.py
+++ addons/crm/tests/test_crm_lead_notification.py
@@ -73,7 +73,6 @@ class NewLeadNotification(TestCrmCommon):
             }, {
                 'name': 'Test Suggestion (partner no email with cc email)',
                 'partner_id': partner_no_email.id,
-                'email_cc': 'test_cc@odoo.com',
                 'user_id': self.user_sales_leads.id
             }
         ])
@@ -148,11 +147,6 @@ class NewLeadNotification(TestCrmCommon):
                     'partner_id': partner_no_email.id,
                     'email': False,
                     'name': 'Test Partner',
-                    'create_values': {},
-                }, {
-                    'name': '',
-                    'email': 'test_cc@odoo.com',
-                    'partner_id': False,
                     'create_values': {},
                 },
             ],

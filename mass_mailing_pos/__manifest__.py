fbf3731543fe127185fdf9b6349c775ddd2c1f60
guce <guce@odoo.com>
2025-12-01 17:34:06 +0100

    [IMP] mass_mailing, *: quicksend mailing to contacts also modified: test_mass_mailing,
    Odoo users can currently select several of their contacts through
    their contact list for a mail to multiple addresses; this opens a
    minimal composer that can serve these needs.
    
    While this serves as a fast way to contact multiple people, it doesn't
    make use of the powerful mass_mailing builder and bundled options.
    
    With this commit, users with mass_mailing installed will instead be
    redirected to the mailing interface, with their selected contacts
    immediately added as the target domain.
    
    Additionally, the Send Email action is now available for any model
    with the '_is_mailing_enabled' attribute set, such as employees or
    mailing lists.
    
    task-5423645

---
+++ mass_mailing_pos/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "mass_mailing_pos",
+    'summary': "Bridge module that unbinds a duplicate action if mass_mailing is installed",
+    'author': "Odoo S.A.",
+    'website': "https://www.odoo.com",
+    'depends': ['mass_mailing', 'point_of_sale'],
+    'auto_install': True,
+    'uninstall_hook': 'uninstall_hook',
+    'data': [
+        'views/pos_order_view.xml',
+    ],
+}

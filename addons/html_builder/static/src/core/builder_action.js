71585f18a9d8e57198791a90a3b7a411c4849d1d
Antoine Guenet <age@odoo.com>
2025-12-04 14:57:04 +0100

    [REF] html_*: add `checkPredicates` util for predicate resources
    \*: `editor`, `builder`.
    
    Introduce a new `checkPredicates` utilitary function on the editor, to
    standardize the way predicates are used in the editor. Without a util,
    predicates are used with `getResource` and then anything goes, meaning
    that sometimes predicates were arbitrarily checked with
    (interchangeably) `Array.some`, `Array.every`, `!Array.some` or with
    ternary logic where `undefined` is given a default value.
    
    With `checkPredicates`, we take the ternary case to be the norm, using
    `every` on the array of predicates that explicitly return a boolean.
    This means:
    - `false` "wins"
    - any predicate function should only respond with a boolean when it has
      something to say about the question
    - any use of `checkPredicate` should define a default boolean return
      value.
    
    Example:
    ```js
    const isTrue = this.checkPredicates("my_predicates", arg1, arg2) ?? true;
    ```
    
    task-5207637

--- addons/html_builder/static/src/core/builder_action.js
+++ addons/html_builder/static/src/core/builder_action.js
@@ -58,6 +58,8 @@ export class BuilderAction {
         this.delegateTo = context.delegateTo;
         /** @type { EditorContext['processThrough'] } **/
         this.processThrough = context.processThrough;
+        /** @type { EditorContext['checkPredicates'] } **/
+        this.checkPredicates = context.checkPredicates;
 
         this.setup();
 

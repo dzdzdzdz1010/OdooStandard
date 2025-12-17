21b564d524baefa18a75cb7fa2b34de1ec86967f
Maxime de Neuville (mane) <mane@odoo.com>
2025-12-17 18:16:39 +0100

    [FIX] project: some fixes after AJU testing

--- addons/hr_timesheet/models/project_task.py
+++ addons/hr_timesheet/models/project_task.py
@@ -59,7 +59,7 @@ class ProjectTask(models.Model):
 
     @api.constrains('project_id')
     def _check_project_root(self):
-        private_tasks = self.filtered(lambda t: not t.project_id)
+        private_tasks = self.filtered(lambda t: not t.project_id and not t.is_template)
         if private_tasks and self.env['account.analytic.line'].sudo().search_count([('task_id', 'in', private_tasks.ids)], limit=1):
             raise UserError(_("This task cannot be private because there are some timesheets linked to it."))
 

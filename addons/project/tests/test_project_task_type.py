11a8fb8a382b3e5ff269784ef49283e4f9b1f2bf
Xavier-Do <xdo@odoo.com>
2025-12-18 09:13:09 +0100

    [REF] project*: move tests post install
    Some tests where running at install by default for historical reasons,
    this commit moves them post install to speedup testing time.

--- addons/project/tests/test_project_task_type.py
+++ addons/project/tests/test_project_task_type.py
@@ -7,7 +7,6 @@ from odoo.tests import tagged
 from odoo.addons.project.tests.test_project_base import TestProjectCommon
 
 
-@tagged('at_install', '-post_install')  # LEGACY at_install
 class TestProjectTaskType(TestProjectCommon):
 
     @classmethod

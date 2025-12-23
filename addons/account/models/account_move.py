fdc6fb4aa79fb4b7516ddbcabd364e9f656e50ed
Harsh Shah <hash@odoo.com>
2025-12-23 10:12:25 +0530

    [IMP] account,*: unify review state widget in bank reconciliation
    *Modules - web, hr_expense
    
    This commit is part of the task applying the new review state widget in bank
    reconciliation, allowing selection of alternative review statuses aligned with
    bank entry behavior.
    
    This commit:
    - Made the review state field required and introduced a new 'no_review' option,
      set as the default value.
    - Updated review state widget usages to properly handle and style the
      'no_review' state.
    - Improved FormViewDialog to support a customHeader passed through props.
    
    task-5420167

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -56,6 +56,7 @@ PAYMENT_STATE_SELECTION = [
         ('invoicing_legacy', 'Invoicing App Legacy'),
 ]
 REVIEW_STATE_SELECTION = [
+    ('no_review', "No Review"),
     ('todo', "To Review"),
     ('reviewed', "Reviewed"),
     ('supervised', "Supervised"),
@@ -313,6 +314,8 @@ class AccountMove(models.Model):
     review_state = fields.Selection(
         string="Review",
         selection=REVIEW_STATE_SELECTION,
+        required=True,
+        default='no_review',
         tracking=True, copy=False,
     )
     posted_before = fields.Boolean(copy=False)
@@ -3859,13 +3862,13 @@ class AccountMove(models.Model):
         for move in self:
             modified_accounting_fields = self._field_will_change_list(move, vals, unmodifiable_fields)
             if vals.get('state') == 'draft' and (
-                ((move.review_state == 'reviewed' or not move.review_state) and not is_user_able_to_review)
+                ((move.review_state in ['no_review', 'reviewed']) and not is_user_able_to_review)
                 or (move.review_state == 'supervised' and not is_user_able_to_supervise)
             ):
                 raise ValidationError(_("This entry has already been reviewed. You need the bookkeeper role to change it."))
             if (vals.get('state') == 'posted' and move.auto_post == 'no') or vals.get('auto_post', 'no') != 'no':
                 if is_user_able_to_review:
-                    if move.review_state:
+                    if move.review_state != 'no_review':
                         move_ids_review_done.append(move.id)
                 else:
                     move_ids_review_todo.append(move.id)

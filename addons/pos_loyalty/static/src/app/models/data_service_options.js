bfd644c7fc84971c26a68da6fcefdc69ab741601
Pedram (PEBR) <pebr@odoo.com>
2025-12-24 16:35:18 +0000

    [PERF] point_of_sale: optimize IndexedDB data synchronization
    Before this commit, if a model had a large number of records, for
    example, loyalty card, the synchronization of IndexedDB could be slow,
    leading to performance issues when adding products to the cart.
    
    opw-5232087
    
    X-original-commit: 4b08139e5de64459cc0be4afbbb608f801c60943

--- addons/pos_loyalty/static/src/app/models/data_service_options.js
+++ addons/pos_loyalty/static/src/app/models/data_service_options.js
@@ -8,9 +8,21 @@ patch(DataServiceOptions.prototype, {
             "loyalty.card": {
                 key: "id",
                 condition: (record) =>
+<<<<<<< 736b71202db840ba6a7ed7e7f014b5b7c493d589
                     record
                         .backLink("<-pos.order.line.coupon_id")
                         .find((l) => !(l.order_id?.finalized && typeof l.order_id.id === "number")),
+||||||| 4490242c2999f9324038b3acbe5a235e75e1854c
+                    record["<-pos.order.line.coupon_id"].find(
+                        (l) => !(l.order_id?.finalized && typeof l.order_id.id === "number")
+                    ),
+=======
+                    record["<-pos.order.line.coupon_id"].find(
+                        (l) => !(l.order_id?.finalized && typeof l.order_id.id === "number")
+                    ),
+                getRecordsBasedOnLines: (orderlines) =>
+                    orderlines.map((line) => line.coupon_id).filter((c) => c),
+>>>>>>> 6c3980da8e3e207b1f1fffc3dd488b960bbe0de2
             },
         };
     },

9592f2883ded07c01ad78b21dfab66ea805e2469
David (dafr) <dafr@odoo.com>
2026-01-14 08:52:17 +0000

    [FIX] mrp_subcontracting_purchase: Singleton Error on 'move_dest_ids.is_subcontract'
    Since commit fc66e2d4eb638f1486e69cd5920f02c787055da1 , a subcontracting MO only has one destination receipt.
    However, Subcontracting MOs created in previous versions can still have multiple dest_move_ids, hence `is_subcontract` must be accessed in a filter or after an `ensure_one()`
    
    OPW-5493343
    
    X-original-commit: 9ea65940c7e2bd37a46b6ff17644e3e3bbf440b6

--- addons/mrp_subcontracting_purchase/models/stock_move.py
+++ addons/mrp_subcontracting_purchase/models/stock_move.py
@@ -10,3 +10,59 @@ class StockMove(models.Model):
     def _is_purchase_return(self):
         res = super()._is_purchase_return()
         return res or self._is_subcontract_return()
+<<<<<<< 09abec9e2b376bdd83b3b5caeff396a2e28d08dd
+||||||| 403f3cf8d291ff2023fc4c5d3631a01465f292c3
+
+    def _get_value_from_account_move(self, quantity, at_date=None):
+        valuation_data = super()._get_value_from_account_move(quantity, at_date=at_date)
+        if not self.production_id or not self.move_dest_ids.is_subcontract or not self.move_dest_ids.purchase_line_id:
+            return valuation_data
+        last_done_receipt = self.move_dest_ids.filtered(lambda m: m.state == 'done')
+        if not last_done_receipt:
+            return valuation_data
+
+        bill_data = last_done_receipt._get_value_from_account_move(quantity)
+        po_data = last_done_receipt._get_value_from_quotation(quantity - bill_data['quantity'])
+        if not bill_data['value'] and not po_data['value']:
+            return valuation_data
+
+        old_extra = self.production_id.extra_cost
+        new_extra_cost = (bill_data['value'] + po_data['value']) / quantity
+
+        # Recompute finished_move price based on quotation and invoice
+        value = (self.price_unit - old_extra + new_extra_cost) * self.quantity
+        return {
+            'value': value,
+            'quantity': quantity,
+            'description': self.env._('%(value)s for %(quantity)s %(unit)s from %(production)s',
+                value=self.company_currency_id.format(self.value), quantity=quantity, unit=self.product_id.uom_id.name,
+                production=self.production_id.display_name),
+        }
+=======
+
+    def _get_value_from_account_move(self, quantity, at_date=None):
+        valuation_data = super()._get_value_from_account_move(quantity, at_date=at_date)
+        last_subcontract_done_receipt = self.move_dest_ids.filtered(
+            lambda m: m.state == 'done' and m.is_subcontract and m.purchase_line_id
+        )
+        if not self.production_id or not last_subcontract_done_receipt:
+            return valuation_data
+
+        bill_data = last_subcontract_done_receipt._get_value_from_account_move(quantity)
+        po_data = last_subcontract_done_receipt._get_value_from_quotation(quantity - bill_data['quantity'])
+        if not bill_data['value'] and not po_data['value']:
+            return valuation_data
+
+        old_extra = self.production_id.extra_cost
+        new_extra_cost = (bill_data['value'] + po_data['value']) / quantity
+
+        # Recompute finished_move price based on quotation and invoice
+        value = (self.price_unit - old_extra + new_extra_cost) * self.quantity
+        return {
+            'value': value,
+            'quantity': quantity,
+            'description': self.env._('%(value)s for %(quantity)s %(unit)s from %(production)s',
+                value=self.company_currency_id.format(self.value), quantity=quantity, unit=self.product_id.uom_id.name,
+                production=self.production_id.display_name),
+        }
+>>>>>>> fd36d856e2abda589d5b8c100793fceb519c88ba

7be40de588c2f48befbd9a8ed4e409ac064fa4bc
Pedram (PEBR) <pebr@odoo.com>
2026-01-19 15:05:22 +0000

    [FIX] point_of_sale: raise error when can not make order paid
    After commit https://github.com/odoo/odoo/commit/dde26e5372b8328bbc9e050f0fdedadd1de649fd, when trying to validate a POS order
    that was not fully paid, the order state would remain in 'draft' state
    but the picking and other related operations would be done. This could
    lead to double entries and inconsistencies.
    
    opw-5447749
    
    X-original-commit: b232bbb721baf3cd10327801c2b11f0e4177094a

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -9,8 +9,15 @@ from uuid import uuid4
 from random import randrange
 from pprint import pformat
 
+<<<<<<< fe1d222d12e78e2282e3ee0a1f78fcbb7153e598
 import psycopg2
 from markupsafe import Markup
+||||||| 79d509a0ab43039cd989b0a70a09231a4e92ba84
+import psycopg2
+import pytz
+=======
+import pytz
+>>>>>>> c3076c8c15e185128c6edc18c4e7833166fd105c
 
 from odoo import api, fields, models, tools, _
 from odoo.tools import float_is_zero, float_round, float_repr, float_compare, formatLang
@@ -138,15 +145,7 @@ class PosOrder(models.Model):
     def _process_saved_order(self, draft):
         self.ensure_one()
         if not draft and self.state != 'cancel':
-            try:
-                self.action_pos_order_paid()
-            except psycopg2.DatabaseError:
-                # do not hide transactional errors, the order(s) won't be saved!
-                raise
-            except UserError as e:
-                _logger.warning('Could not fully process the POS Order: %s', tools.exception_to_unicode(e))
-            except Exception as e:
-                _logger.error('Could not fully process the POS Order: %s', tools.exception_to_unicode(e), exc_info=True)
+            self.action_pos_order_paid()
             self._create_order_picking()
             self._compute_total_cost_in_real_time()
 

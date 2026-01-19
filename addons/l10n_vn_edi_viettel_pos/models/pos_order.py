425f45eb408f49503ca35a427722d432db3f35fe
jand-odoo <jand@odoo.com>
2026-01-19 20:39:56 +0800

    [ADD] l10n_vn_edi_viettel_pos: SInvoice integration for PoS
    Add Viettel SInvoice integration with the Point of Sale
    module to support PoS e-invoicing for the Vietnamese market.
    
    task-4844619

---
+++ addons/l10n_vn_edi_viettel_pos/models/pos_order.py
@@ -0,0 +1,42 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields, api
+
+
+class PosOrder(models.Model):
+    _inherit = 'pos.order'
+
+    credit_note_reason = fields.Char(string='Credit Note Reason', copy=False)
+    l10n_vn_has_sinvoice_pdf = fields.Boolean(compute="_compute_sinvoice_has_pdf", string="SInvoice PDF Available")
+    l10n_vn_sinvoice_state = fields.Selection(related='account_move.l10n_vn_edi_invoice_state')
+
+    @api.depends("account_move.l10n_vn_edi_sinvoice_pdf_file")
+    def _compute_sinvoice_has_pdf(self):
+        for record in self:
+            record['l10n_vn_has_sinvoice_pdf'] = bool(record.account_move.l10n_vn_edi_sinvoice_pdf_file)
+
+    def _prepare_invoice_vals(self):
+        vals = super()._prepare_invoice_vals()
+
+        if self.country_code != 'VN' or not self.config_id.l10n_vn_auto_send_to_sinvoice:
+            return vals
+
+        sinvoice_symbol = self.config_id.l10n_vn_pos_symbol or self.config_id.company_id.l10n_vn_pos_default_symbol
+        if sinvoice_symbol:
+            vals['l10n_vn_edi_invoice_symbol'] = sinvoice_symbol.id
+
+            # Refund Invoice (Credit Note)
+            if self.amount_total < 0:
+                vals['l10n_vn_edi_adjustment_type'] = '1'  # Money Adjustment
+
+                reason = self.credit_note_reason
+                current_ref = vals.get('ref')
+                if current_ref and reason:
+                    vals['ref'] = f"{current_ref}, {reason}"
+
+        return vals
+
+    def _create_invoice(self, move_vals):
+        if self.country_code == 'VN' and self.config_id.l10n_vn_auto_send_to_sinvoice:
+            return super()._create_invoice(move_vals).with_context(skip_fetch_sinvoice_files=True)
+        return super()._create_invoice(move_vals)
---
+++ addons/l10n_vn_edi_viettel_pos/models/res_company.py
@@ -0,0 +1,16 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields, api
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    l10n_vn_pos_default_symbol = fields.Many2one(
+        comodel_name='l10n_vn_edi_viettel.sinvoice.symbol',
+        string='Default PoS Symbol',
+    )
+
+    @api.model
+    def _load_pos_data_fields(self, config_id):
+        return super()._load_pos_data_fields(config_id) + ['l10n_vn_pos_default_symbol']

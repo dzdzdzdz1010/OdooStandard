c3a70072c3cfbda1690416377a1d21dc97a9a46f
William Henrotin <whe@odoo.com>
2026-01-20 11:56:45 +0100

    [IMP] mrp: enhance workorder planning

--- addons/mrp/models/mrp_workorder.py
+++ addons/mrp/models/mrp_workorder.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 import json
 
 from odoo import Command, _, api, fields, models
+from odoo.fields import Domain
 from odoo.exceptions import UserError, ValidationError
 from odoo.tools import format_datetime, float_round
 from odoo.tools.date_utils import sum_intervals
@@ -566,63 +567,80 @@ class MrpWorkorder(models.Model):
     def _get_byproduct_move_to_update(self):
         return self.production_id.move_finished_ids.filtered(lambda x: (x.product_id.id != self.production_id.product_id.id) and (x.state not in ('done', 'cancel')))
 
-    def _plan_workorder(self, replan=False):
-        self.ensure_one()
-        # Plan workorder after its predecessors
-        replan_predecessors = replan
-        if self.state not in ['blocked', 'ready']:
-            replan = False
-        elif replan:
-            self.leave_id.unlink()
-        elif not self.leave_id:
-            replan = True
-        date_start = max(self.production_id.date_start, datetime.now())
-        for workorder in self.blocked_by_workorder_ids:
-            workorder._plan_workorder(replan_predecessors)
-            if workorder.date_finished and workorder.date_finished > date_start:
-                date_start = workorder.date_finished
-        # Plan only suitable workorders
-        if not replan:
+    def _plan_workorders(self, from_mo=False, from_workorder=False, from_date=False):
+        """Plan or replan a set of manufacturing workorders
+
+        :param from_workorder: An optional `mrp.workorder` recordset. If provided,
+            all workorders in the same workcenter that are scheduled to start on or after
+            this workorder's planned start date will also be included in the replanning.
+        :param from_date: An optional `datetime` object. If provided, all workorders
+            scheduled to start on or after this date will be included in the replanning,
+            regardless of workcenter.
+        :type workorder: mrp.workorder or None
+        :type date: datetime.datetime or None
+        :raises UserError: If a cyclic dependency is detected among workorders, or if it's
+            impossible to find an available time slot for a workorder.
+        """
+        workorders_to_plan = False
+        if self:
+            workorders_to_plan = self
+        elif from_mo:
+            workorders_to_plan = from_mo.workorder_ids
+        elif from_workorder:
+            self.env['mrp.workorder'].search(Domain.OR([
+                [('date_start', '=', False)],
+                [('date_start', '>', from_workorder.date_start)],
+            ]))
+        elif from_date:
+            workorders_to_plan = self.env['mrp.workorder'].search([
+                ('date_start', '>=', from_date),
+            ])
+        if not workorders_to_plan:
             return
-        # Consider workcenter and alternatives
-        workcenters = self.workcenter_id | self.workcenter_id.alternative_workcenter_ids
-        best_date_finished = datetime.max
-        vals = {}
-        for workcenter in workcenters:
-            if not workcenter.resource_calendar_id:
-                raise UserError(_('There is no defined calendar on workcenter %s.', workcenter.name))
-            # Compute theoretical duration
-            if self.workcenter_id == workcenter:
-                duration_expected = self.duration_expected
-            else:
-                duration_expected = self._get_duration_expected(alternative_workcenter=workcenter)
-            from_date, to_date = workcenter._get_first_available_slot(date_start, duration_expected)
-            # If the workcenter is unavailable, try planning on the next one
-            if not from_date:
-                continue
-            # Check if this workcenter is better than the previous ones
-            if to_date and to_date < best_date_finished:
-                best_date_start = from_date
-                best_date_finished = to_date
-                best_workcenter = workcenter
-                vals = {
-                    'workcenter_id': workcenter.id,
-                    'duration_expected': duration_expected,
-                }
-        # If none of the workcenter are available, raise
-        if best_date_finished == datetime.max:
-            raise UserError(_('Impossible to plan the workorder. Please check the workcenter availabilities.'))
-        # Create leave on chosen workcenter calendar
-        leave = self.env['resource.calendar.leaves'].create({
-            'name': self.display_name,
-            'calendar_id': best_workcenter.resource_calendar_id.id,
-            'date_from': best_date_start,
-            'date_to': best_date_finished,
-            'resource_id': best_workcenter.resource_id.id,
-            'time_type': 'other'
-        })
-        vals['leave_id'] = leave.id
-        self.write(vals)
+        date_start = max(self.production_id.date_start, datetime.now())
+        for wo in workorders_to_plan:
+            wo.blocked_by_workorder_ids._plan_workorders()
+            date_start = wo.blocked_by_workorder_ids[-1].date_finished
+            # Consider workcenter and alternatives
+            workcenters = wo.workcenter_id | wo.workcenter_id.alternative_workcenter_ids
+            best_date_finished = datetime.max
+            vals = {}
+            for workcenter in workcenters:
+                if not workcenter.resource_calendar_id:
+                    raise UserError(_('There is no defined calendar on workcenter %s.', workcenter.name))
+                # Compute theoretical duration
+                if self.workcenter_id == workcenter:
+                    duration_expected = self.duration_expected
+                else:
+                    duration_expected = self._get_duration_expected(alternative_workcenter=workcenter)
+                from_date, to_date = workcenter._get_first_available_slot(date_start, duration_expected)
+                # If the workcenter is unavailable, try planning on the next one
+                if not from_date:
+                    continue
+                # Check if this workcenter is better than the previous ones
+                if to_date and to_date < best_date_finished:
+                    best_date_start = from_date
+                    best_date_finished = to_date
+                    best_workcenter = workcenter
+                    vals = {
+                        'workcenter_id': workcenter.id,
+                        'duration_expected': duration_expected,
+                    }
+            # If none of the workcenter are available, raise
+            if best_date_finished == datetime.max:
+                raise UserError(_('Impossible to plan the workorder. Please check the workcenter availabilities.'))
+            # Create leave on chosen workcenter calendar
+            wo.write({
+                **vals,
+                'leave_id': [Command.create({
+                    'name': self.display_name,
+                    'calendar_id': best_workcenter.resource_calendar_id.id,
+                    'date_from': best_date_start,
+                    'date_to': best_date_finished,
+                    'resource_id': best_workcenter.resource_id.id,
+                    'time_type': 'other',
+                })]
+            })
 
     def _cal_cost(self, date=False):
         """Returns total cost of time spent on workorder.

55cd6476f9c3a6e1396e64f357a7978b9c13bc68
Ruben Gomes (rugo) <rugo@odoo.com>
2026-01-20 15:09:44 +0100

    [MOV] core: move test_image from base/tests to test_tools/tests

--- odoo/addons/base/tests/__init__.py
+++ odoo/addons/base/tests/__init__.py
@@ -16,7 +16,6 @@ from . import test_func
 from . import test_groups
 from . import test_http_case
 from . import test_i18n
-from . import test_image
 from . import test_install
 from . import test_avatar_mixin
 from . import test_init
--- odoo/addons/test_tools/tests/__init__.py
+++ odoo/addons/test_tools/tests/__init__.py
@@ -1,3 +1,4 @@
 from . import test_barcode
 from . import test_date_utils
 from . import test_float_utils
+from . import test_image

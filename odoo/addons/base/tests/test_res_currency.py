8fa2bcf5678a2abeb845b8591320a8391efb14bb
Ruben Gomes (rugo) <rugo@odoo.com>
2026-01-20 15:05:37 +0100

    [MOV] core: move test_float from base/tests to test_tools/tests
    * Move test_rounding_04 from test_float to test_res_currency
    * Move all other test_float tests from base/tests to test_tools/tests
    * Remove test_float_utils dependency to res.currency

--- odoo/addons/base/tests/test_res_currency.py
+++ odoo/addons/base/tests/test_res_currency.py
@@ -110,3 +110,23 @@ class TestResCurrency(TransactionCase):
         amount_test = currency.amount_to_text(0.28)
         self.assertNotEqual(amount_test, amount_target,
                             "Amount in text should not depend on float representation")
+
+    def test_rounding_04(self):
+        """ check that proper rounding is performed for float persistence """
+        currency = self.env.ref('base.EUR')
+        currency_rate = self.env['res.currency.rate']
+
+        def try_roundtrip(value, expected, date):
+            rate = currency_rate.create({'name': date,
+                                         'rate': value,
+                                         'currency_id': currency.id})
+            self.assertEqual(rate.rate, expected,
+                             'Roundtrip error: got %s back from db, expected %s' % (rate, expected))
+
+        # res.currency.rate no more uses 6 digits of precision by default, it now uses whatever precision it gets
+        try_roundtrip(10000.999999, 10000.999999, '2000-01-03')
+
+        #TODO re-enable those tests when tests are made on dedicated models
+        # (res.currency.rate don't accept negative value anymore)
+        #try_roundtrip(-2.6748955, -2.674896, '2000-01-02')
+        #try_roundtrip(-10000.999999, -10000.999999, '2000-01-04')
--- odoo/addons/test_tools/tests/__init__.py
+++ odoo/addons/test_tools/tests/__init__.py
@@ -1,2 +1,3 @@
 from . import test_barcode
 from . import test_date_utils
+from . import test_float_utils

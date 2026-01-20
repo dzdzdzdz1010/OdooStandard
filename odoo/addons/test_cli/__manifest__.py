d674cf0fee6357899070b76b4859ca8ec11ff4f4
Ruben Gomes (rugo) <rugo@odoo.com>
2026-01-20 14:42:15 +0100

    [MOV] core: move test_cli from base/tests to test_cli/tests

---
+++ odoo/addons/test_cli/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Test CLI',
+    'category': 'Hidden/Tests',
+    'description': """A module to test the CLI.""",
+    'depends': ['base'],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}

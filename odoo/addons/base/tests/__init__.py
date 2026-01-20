7e4b0ff144ba04fdcd51ad8736b9455a2754d588
Ruben Gomes (rugo) <rugo@odoo.com>
2026-01-20 15:16:50 +0100

    [MOV] core: move test_cloc from base/tests to test_tools/tests

--- odoo/addons/base/tests/__init__.py
+++ odoo/addons/base/tests/__init__.py
@@ -66,7 +66,6 @@ from . import test_test_suite
 from . import test_tests_tags
 from . import test_transactions
 from . import test_form_create
-from . import test_cloc
 from . import test_profiler
 from . import test_pdf
 from . import test_neutralize

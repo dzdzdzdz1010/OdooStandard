8ed99ff08039839fa71fff843050af60abf2c55f
Ruben Gomes (rugo) <rugo@odoo.com>
2026-01-21 10:17:41 +0100

    [MOV] core: move test_basecase from base/tests to test_tests/tests

---
+++ odoo/addons/test_tests/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Test Tests',
+    'category': 'Hidden/Tests',
+    'description': """A module to test the Tests.""",
+    'depends': ['base'],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}

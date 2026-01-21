d7f30841dde5fb0591e99a1c2cb9645236a50868
Ruben Gomes (rugo) <rugo@odoo.com>
2026-01-21 11:08:53 +0100

    [MOV] core: move test_neutralize from base/tests to test_modules/tests

---
+++ odoo/addons/test_modules/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Test Modules',
+    'category': 'Hidden/Tests',
+    'description': """A module to test the Modules.""",
+    'depends': ['base'],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}

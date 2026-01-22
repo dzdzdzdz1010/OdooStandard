55c33ba2bb293c9a3ee997c855fce8c0d8c1fabf
Ruben Gomes (rugo) <rugo@odoo.com>
2026-01-22 15:22:58 +0100

    [REF] core: rename test_split_table to test_ir_actions_report

--- odoo/addons/base/tests/__init__.py
+++ odoo/addons/base/tests/__init__.py
@@ -30,7 +30,7 @@ from . import test_ir_qweb
 from . import test_res_config
 from . import test_res_lang
 from . import test_search
-from . import test_split_table
+from . import test_ir_actions_report
 from . import test_translate
 from . import test_tz
 # from . import test_uninstall  # loop

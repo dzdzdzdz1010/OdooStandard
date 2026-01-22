8de511caedf17a1f54345ca40171c2b1cb31c16d
pssh-odoo <pssh@odoo.com>
2026-01-22 18:49:21 +0530

    [IMP] l10n_sa_edi: remove l10n_sa_is_retention field
    Before this commit:
    - The user needed to use the boolean field 'l10n_sa_is_retention' on a negative
      tax for it to be considered a retention tax. This complicates the flow, as any
      negative tax is a retention tax by default, so the need for the checkbox is
      unnecessary.
    
    After this commit:
    - The boolean field 'l10n_sa_is_retention' is removed.
    
    Related: https://github.com/odoo/upgrade/pull/9351
    task-5865831

--- addons/l10n_sa_edi/models/account_move.py
+++ addons/l10n_sa_edi/models/account_move.py
@@ -331,7 +331,8 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     def _apply_retention_tax_filter(self, tax_values):
-        return not tax_values['tax_id'].l10n_sa_is_retention
+        tax = tax_values['tax_id']
+        return (tax.amount < 0 and not tax.is_withholding_tax_on_payment)
 
     def _is_global_discount_line(self):
         """
@@ -357,5 +358,8 @@ class AccountMoveLine(models.Model):
                 line.l10n_gcc_invoice_tax_amount = sum(
                     tax_data['tax_amount_currency']
                     for tax_data in base_line['tax_details']['taxes_data']
-                    if not tax_data['tax'].l10n_sa_is_retention
+                    if not (
+                        tax_data['tax'].amount < 0
+                        and not tax_data['tax'].is_withholding_tax_on_payment
+                    )
                 )

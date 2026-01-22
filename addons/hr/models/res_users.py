0fdf06c17090a7b5aa8ea74d3aae3cea4d7cb177
romc-odoo <romc@odoo.com>
2026-01-22 10:55:19 +0100

    [REF] hr: merge homeworking module into hr
    This commit merges the hr_homeworking module into the hr module.
    
    task-5089346

--- addons/hr/models/res_users.py
+++ addons/hr/models/res_users.py
@@ -9,6 +9,8 @@ from odoo.exceptions import AccessError
 from odoo.fields import Domain
 from odoo.tools.misc import clean_context
 
+from .hr_employee_location import DAYS
+
 
 def field_employee(field_type: type[fields.Field], name: str, *, field_name='', user_writeable=False, **kw):
     """Simulating a related field "employee_id.{name}".
@@ -119,6 +121,25 @@ class ResUsers(models.Model):
     is_system = fields.Boolean(compute="_compute_is_system")
     is_hr_user = fields.Boolean(compute='_compute_is_hr_user')
 
+    monday_location_id = fields.Many2one("hr.work.location", related="employee_id.monday_location_id", readonly=False, string='Mondays', user_writeable=True)
+    tuesday_location_id = fields.Many2one("hr.work.location", related="employee_id.tuesday_location_id", readonly=False, string='Tuesdays', user_writeable=True)
+    wednesday_location_id = fields.Many2one("hr.work.location", related="employee_id.wednesday_location_id", readonly=False, string='Wednesdays', user_writeable=True)
+    thursday_location_id = fields.Many2one("hr.work.location", related="employee_id.thursday_location_id", readonly=False, string='Thursdays', user_writeable=True)
+    friday_location_id = fields.Many2one("hr.work.location", related="employee_id.friday_location_id", readonly=False, string='Fridays', user_writeable=True)
+    saturday_location_id = fields.Many2one("hr.work.location", related="employee_id.saturday_location_id", readonly=False, string='Saturdays', user_writeable=True)
+    sunday_location_id = fields.Many2one("hr.work.location", related="employee_id.sunday_location_id", readonly=False, string='Sundays', user_writeable=True)
+
+    def _compute_im_status(self):
+        super()._compute_im_status()
+        dayfield = self.env['hr.employee']._get_current_day_location_field()
+        for user in self:
+            location_type = user[dayfield].location_type
+            if not location_type:
+                continue
+            im_status = user.im_status
+            if im_status in ["online", "away", "busy", "offline"]:
+                user.im_status = "presence_" + location_type + "_" + im_status
+
     @api.depends_context('uid')
     def _compute_is_system(self):
         self.is_system = self.env.user._is_system()
@@ -160,7 +181,7 @@ class ResUsers(models.Model):
     def _get_employee_fields_to_sync(self):
         """Get values to sync to the related employee when the User is changed.
         """
-        return ['name', 'email', 'image_1920', 'tz']
+        return ['name', 'email', 'image_1920', 'tz'] + DAYS
 
     def _get_personal_info_partner_ids_to_notify(self, employee):
         if employee.version_id.hr_responsible_id:
--- addons/hr_holidays/tests/test_out_of_office.py
+++ addons/hr_holidays/tests/test_out_of_office.py
@@ -114,21 +114,21 @@ class TestOutOfOfficePerformance(TestHrHolidaysCommon, TransactionCaseWithUserDe
     @users('__system__', 'demo')
     @warmup
     def test_leave_im_status_performance_partner_offline(self):
-        with self.assertQueryCount(__system__=4, demo=4):
+        with self.assertQueryCount(__system__=6, demo=6):
             self.assertEqual(self.employer_partner.im_status, 'offline')
 
     @users('__system__', 'demo')
     @warmup
     def test_leave_im_status_performance_user_leave_offline(self):
         self.leave.write({'state': 'validate'})
-        with self.assertQueryCount(__system__=2, demo=2):
+        with self.assertQueryCount(__system__=4, demo=4):
             self.assertEqual(self.hr_user.im_status, 'leave_offline')
 
     @users('__system__', 'demo')
     @warmup
     def test_leave_im_status_performance_partner_leave_offline(self):
         self.leave.write({'state': 'validate'})
-        with self.assertQueryCount(__system__=4, demo=4):
+        with self.assertQueryCount(__system__=6, demo=6):
             self.assertEqual(self.hr_partner.im_status, 'leave_offline')
 
     def test_search_absent_employee(self):

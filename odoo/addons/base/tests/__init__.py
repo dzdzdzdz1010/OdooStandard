c92320515d98ac266184cc752144a86e254689a4
Ruben Gomes (rugo) <rugo@odoo.com>
2026-01-23 09:49:36 +0100

    [MOV] core: move test_test_retry, test_test_suite and test_tests_tags from base/tests to test_tests/tests

--- odoo/addons/base/tests/__init__.py
+++ odoo/addons/base/tests/__init__.py
@@ -44,9 +44,6 @@ from . import test_res_partner_bank
 from . import test_res_partner_merge
 from . import test_res_users
 from . import test_reports
-from . import test_test_retry
-from . import test_test_suite
-from . import test_tests_tags
 from . import test_transactions
 from . import test_form_create
 from . import test_res_groups
--- odoo/addons/test_tests/tests/__init__.py
+++ odoo/addons/test_tests/tests/__init__.py
@@ -1,1 +1,4 @@
 from . import test_common
+from . import test_test_retry
+from . import test_test_suite
+from . import test_tests_tags

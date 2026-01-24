5b0a5986c7c7b8f72d9ddf842784e8a69ba2fe68
abd-msyukyu-odoo <abd@odoo.com>
2026-01-24 17:07:40 +0100

    test

--- addons/mass_mailing/static/src/builder/plugins/customize_mailing_plugin.js
+++ addons/mass_mailing/static/src/builder/plugins/customize_mailing_plugin.js
@@ -5,6 +5,7 @@ import { memoize } from "@web/core/utils/functions";
 import { CUSTOMIZE_MAILING_VARIABLES } from "@mass_mailing/builder/plugins/customize_mailing_variables";
 import { CUSTOMIZE_MAILING_VARIABLES_DEFAULTS } from "./customize_mailing_variables";
 import { splitSelectorList } from "@mail/convert_inline/style_utils";
+import { parseCssText } from "@mail/convert_inline/css_parsers";
 
 const RE_SELECTOR_ENDS_WITH_GT_STAR = />\s*\*\s*$/;
 export const PRIORITY_STYLES = {
@@ -114,7 +115,10 @@ export class CustomizeMailingPlugin extends Plugin {
         const rules = [...styleEl.sheet.cssRules];
         for (const rule of rules) {
             for (const selector of splitSelectorList(rule.selectorText)) {
-                for (const property of rule.style) {
+                const propertyNames = parseCssText(rule.style.cssText).map(
+                    (property) => property.name
+                );
+                for (const property of propertyNames) {
                     const selectors =
                         property !== "font-family"
                             ? [selector]

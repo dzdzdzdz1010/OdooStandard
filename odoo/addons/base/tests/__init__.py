fab3974186c2a3ebaaee20497c1a021426b2953a
Ruben Gomes (rugo) <rugo@odoo.com>
2026-01-27 09:55:44 +0100

    [MOV] core: test_reports to test_ir_actions_report

--- odoo/addons/base/tests/__init__.py
+++ odoo/addons/base/tests/__init__.py
@@ -33,5 +33,4 @@ from . import test_res_partner
 from . import test_res_partner_bank
 from . import test_res_partner_merge
 from . import test_res_users
-from . import test_reports
 from . import test_res_groups

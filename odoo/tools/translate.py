bce5a04196affcbc58c3a183deb516d28df952a9
Sébastien (blse) <blse@odoo.com>
2026-01-27 15:10:27 +0100

    remove logic and test about o_translate_inline

--- odoo/tools/translate.py
+++ odoo/tools/translate.py
@@ -153,13 +153,7 @@ def translate_xml_node(node, callback, parse, serialize):
     def translatable(node):
         """ Return whether the given node can be translated as a whole. """
         return (
-            # Some specific nodes (e.g., text highlights) have an auto-updated
-            # DOM structure that makes them impossible to translate.
-            # The introduction of a translation `<span>` in the middle of their
-            # hierarchy breaks their functionalities. We need to force them to
-            # be translated as a whole using the `o_translate_inline` class.
-            "o_translate_inline" in node.attrib.get("class", "").split()
-            or node.tag in TRANSLATED_ELEMENTS
+            node.tag in TRANSLATED_ELEMENTS
             and not any(key.startswith("t-") or key.endswith(".translate") or key == "groups" for key in node.attrib)
             and all(translatable(child) for child in node)
         )

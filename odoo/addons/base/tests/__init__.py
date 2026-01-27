2fb1f9e53292c9a5f42bfd26a2007032638ccb18
Ruben Gomes (rugo) <rugo@odoo.com>
2026-01-27 10:04:10 +0100

    [MOV] core: move test_module from base/tests to test_modules/tests

--- odoo/addons/base/tests/__init__.py
+++ odoo/addons/base/tests/__init__.py
@@ -19,7 +19,6 @@ from . import test_ir_sequence
 from . import test_ir_embedded_actions
 from . import test_ir_default
 from . import test_ir_ui_menu
-from . import test_module
 from . import test_ir_qweb_fields
 from . import test_ir_qweb
 from . import test_res_config
--- odoo/addons/test_modules/tests/__init__.py
+++ odoo/addons/test_modules/tests/__init__.py
@@ -1,2 +1,3 @@
+from . import test_module
 from . import test_module_graph
 from . import test_neutralize

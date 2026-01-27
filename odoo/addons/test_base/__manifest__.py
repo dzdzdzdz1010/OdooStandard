791cc1d353a1234f108c0eeea1723f0bf43170ae
Ruben Gomes (rugo) <rugo@odoo.com>
2026-01-27 15:23:50 +0100

    [MOV] core: move test_res_config from test_test_utilities/tests to test_base/tests

---
+++ odoo/addons/test_base/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Test Base',
+    'category': 'Hidden/Tests',
+    'description': """A module to test the Base module.""",
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}

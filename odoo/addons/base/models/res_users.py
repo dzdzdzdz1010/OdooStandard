f5555e44a735570eb9ab63c52f87af2044459106
Paul Morelle <pmo@odoo.com>
2026-01-28 14:07:30 +0100

    [IMP] base: allow programmatic creation of API keys
    Previously, API keys could only be created interactively: the method
    required an interactive session and a recently typed password.
    
    This commit adds a new method on `res.users.apikeys` that allows
    creating an API key when an existing one is provided. Optionally, in the
    case of a renewal, the new key can replace the old one, and the provided
    one is unlinked.

--- odoo/addons/base/models/res_users.py
+++ odoo/addons/base/models/res_users.py
@@ -25,7 +25,7 @@ from odoo.api import SUPERUSER_ID
 from odoo.exceptions import AccessDenied, AccessError, UserError, ValidationError
 from odoo.fields import Command, Domain
 from odoo.http import request, DEFAULT_LANG
-from odoo.tools import email_domain_extract, is_html_empty, frozendict, reset_cached_properties, SQL
+from odoo.tools import email_domain_extract, is_html_empty, frozendict, reset_cached_properties, str2bool, SQL
 
 
 _logger = logging.getLogger(__name__)
@@ -1513,6 +1513,7 @@ KEY_CRYPT_CONTEXT = CryptContext(
     # attacks on API keys isn't much of a concern
     ['pbkdf2_sha512'], pbkdf2_sha512__rounds=6000,
 )
+DEFAULT_PROGRAMMATIC_API_KEYS_LIMIT = 10
 
 
 class ResUsersApikeys(models.Model):
@@ -1566,6 +1567,7 @@ class ResUsersApikeys(models.Model):
             _logger.info("API key(s) removed: scope: <%s> for '%s' (#%s) from %s",
                self.mapped('scope'), self.env.user.login, self.env.uid, ip)
             self.sudo().unlink()
+            self.env.registry.clear_cache()
             return {'type': 'ir.actions.act_window_close'}
         raise AccessError(_("You can not remove API keys unless they're yours or you are a system user"))
 
@@ -1627,6 +1629,73 @@ class ResUsersApikeys(models.Model):
             self._description, scope, self.env.user.login, self.env.uid, ip)
 
         return k
+
+    @api.model
+    def generate(self, key, scope, name, expiration_date):
+        """
+        Generate a new API key with an existing API key.
+        The provided `key` must be an existing API key that belongs to the current user.
+        Its scope must be compatible with `scope`.
+        The `expiration_date` must be allowed for the user's group.
+
+        To renew a key, generate the new one, store it, and then call `revoke` on the previous one.
+        """
+        ICP = self.env['ir.config_parameter'].sudo()
+
+        if not str2bool(ICP.get_param('base.enable_programmatic_api_keys', False)):
+            raise UserError(_("Programmatic API keys are not enabled"))
+        if not isinstance(expiration_date, datetime.datetime):
+            expiration_date = fields.Datetime.from_string(expiration_date)
+
+        nb_keys = self.search_count([('user_id', '=', self.env.uid), ('expiration_date', '>=', self.env.cr.now())])
+        try:
+            nb_keys_limit = int(ICP.get_param('base.programmatic_api_keys_limit', DEFAULT_PROGRAMMATIC_API_KEYS_LIMIT))
+        except ValueError:
+            _logger.warning("Invalid value for 'base.programmatic_api_keys_limit', using default value.")
+            nb_keys_limit = DEFAULT_PROGRAMMATIC_API_KEYS_LIMIT
+        if nb_keys >= nb_keys_limit:
+            raise UserError(_('Limit of %s API keys is reached', nb_keys_limit))
+
+        # Scope compatibility rules:
+        # - A global key can generate credentials for any scope (including global).
+        # - A scoped key can only generate credentials for its own scope.
+        #
+        # This is enforced in _check_credentials by validating scope usage,
+        # and the validated scope is then reused when calling _generate.
+
+        logout_env = api.Environment(self.env.cr, None, {})
+        uid = logout_env['res.users.apikeys']._check_credentials(scope=scope or 'rpc', key=key)
+        if not uid or uid != self.env.uid:
+            raise UserError(_("The provided API key is invalid or does not belong to the current user"))
+        new_key = self._generate(scope, name, expiration_date)
+        _logger.info("%s %r generated from %r", self._description, new_key[:INDEX_SIZE], key[:INDEX_SIZE])
+
+        return {'result': new_key}
+
+    @api.model
+    def revoke(self, key):
+        """
+        Revoke an existing API key.
+        If it exists, the `key` will be removed from the server.
+        """
+        if not self.env['ir.config_parameter'].sudo().get_param('base.enable_programmatic_api_keys'):
+            raise UserError(_("Programmatic API keys are not enabled"))
+        assert key, "key required"
+        self.env.cr.execute(f'''
+            SELECT id, key
+            FROM {self._table}
+            WHERE
+                index = %s
+                AND (
+                    expiration_date IS NULL OR
+                    expiration_date >= now() at time zone 'utc'
+                )
+        ''', [key[:INDEX_SIZE]])
+        for key_id, current_key in self.env.cr.fetchall():
+            if key and KEY_CRYPT_CONTEXT.verify(key, current_key):
+                self.env['res.users.apikeys'].browse(key_id)._remove()
+                return {'result': True}
+        raise AccessDenied(_("The provided API key is invalid."))
 
     @api.autovacuum
     def _gc_user_apikeys(self):

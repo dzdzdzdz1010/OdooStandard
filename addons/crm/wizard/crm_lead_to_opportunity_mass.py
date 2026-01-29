43af93b25d7dfb4a37b0481c23530bf64dc4dfcc
Frantisek Binovsky <frbin@odoo.com>
2026-01-29 15:15:27 +0100

    [FIX] sales_team: fix _get_default_team_id newId argument
    The user_id argument passed to _get_default_team_id was of type <NewId>. A NewId
    object evaluates as false in the `if not user_id` condition, incorrectly causing
    the current env user to be returned every time (passing the newId to the browse
    call in the else branch doesn't retrieve the user either).
    
    Fixed by passing the integer representation of the id from the .ids property.
    
    Task-5892533

--- addons/crm/wizard/crm_lead_to_opportunity_mass.py
+++ addons/crm/wizard/crm_lead_to_opportunity_mass.py
@@ -71,7 +71,7 @@ class CrmLead2opportunityPartnerMass(models.TransientModel):
             user = convert.user_ids and convert.user_ids[0] or self.env.user
             if convert.team_id and user in convert.team_id.member_ids | convert.team_id.user_id:
                 continue
-            team = self.env['crm.team']._get_default_team_id(user_id=user.id, domain=None)
+            team = self.env['crm.team']._get_default_team_id(user_id=user.ids[0], domain=None)
             convert.team_id = team.id
 
     @api.depends('lead_tomerge_ids')

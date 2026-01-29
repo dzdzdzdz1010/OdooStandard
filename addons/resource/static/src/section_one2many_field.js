f80bdfe250536797c254914f48ad60c1f5dfdcba
FedericoBraidi <brfe@odoo.com>
2026-01-29 14:27:31 +0100

    [REM] resource, hr_holidays: remove display_type field
    A display_type field on resource_calendar_attendance was not used
    anymore. With this PR we remove it and we refactor the parts of code
    where it was still referenced.
    
    Task: 5886926

--- addons/resource/static/src/section_one2many_field.js
+++ addons/resource/static/src/section_one2many_field.js
@@ -1,11 +1,9 @@
-import { SectionListRenderer } from "./section_list_renderer";
 import { registry } from "@web/core/registry";
 import { X2ManyField, x2ManyField } from "@web/views/fields/x2many/x2many_field";
 
 class SectionOneToManyField extends X2ManyField {
     static components = {
         ...X2ManyField.components,
-        ListRenderer: SectionListRenderer,
     };
     static defaultProps = {
         ...X2ManyField.defaultProps,

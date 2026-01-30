e8e67444702a0bfa72032357eb39fe18f652ad05
Ze Chen (zeche) <zeche@odoo.com>
2026-01-30 13:49:40 -0500

    [FIX] website_event_sale: Prevent auto registration
    **Before this commit**
    If a gift card balance fully covers a shopping cart containing multiple
    events, Odoo auto-confirms the order as soon as the last event is added,
    skipping the final checkout step.
    
    **After this commit**
    Sale orders will no longer be automatically confirmed when a customer
    registers for a paid event, even if an applied gift card brings the
    total balance to zero.
    
    opw-5896626

--- addons/website_event_sale/tests/test_website_event_sale.py
+++ addons/website_event_sale/tests/test_website_event_sale.py
@@ -61,3 +61,41 @@ class TestWebsiteEventSale(HttpCaseWithUserPortal, TestWebsiteEventSaleCommon):
             ('order_line.event_ticket_id', '=', self.ticket.id),
             ('order_line.event_ticket_id', '=', free_ticket.id)
         ]), "Sale order should be created for the free/paid tickets mix")
+
+    def test_website_event_sale_giftcard_covers_full_cost(self):
+        """Test saleorder is not auto confirmed if the gift card in the card can fully cover the cost"""
+        self.authenticate(None, None)
+        event_questions = self.event.question_ids
+        event_registration_count = len(self.event.registration_ids)
+        name_question = event_questions.filtered(lambda q: q.question_type == 'name')
+        email_question = event_questions.filtered(lambda q: q.question_type == 'email')
+        phone_question = event_questions.filtered(lambda q: q.question_type == 'phone')
+
+        self.url_open(f'/event/{self.event.id}/registration/confirm', data={
+            f'1-name-{name_question.id}': 'Bob',
+            f'1-email-{email_question.id}': 'bob@test.lan',
+            f'1-phone-{phone_question.id}': '8989898989',
+            '1-event_ticket_id': self.ticket.id,
+            'csrf_token': http.Request.csrf_token(self),
+        })
+
+        gift_card = self.env['loyalty.card'].create({
+            'program_id': self.env.ref('loyalty.gift_card_program').id,
+            'points': 200.0,
+        })
+
+        self.url_open('/shop/pricelist', data={
+            'promo': gift_card.code,
+            'csrf_token': http.Request.csrf_token(self),
+            })
+
+        self.url_open(f'/event/{self.event.id}/registration/confirm', data={
+            f'1-name-{name_question.id}': 'Bob',
+            f'1-email-{email_question.id}': 'bob@test.lan',
+            f'1-phone-{phone_question.id}': '8989898989',
+            '1-event_ticket_id': self.ticket.id,
+            'csrf_token': http.Request.csrf_token(self),
+        })
+        last_so = self.env['sale.order'].search([], order='id desc', limit=1)
+        self.assertEqual(len(self.event.registration_ids), event_registration_count + 2)
+        self.assertTrue(last_so.state == 'draft', "The status of upaid events should be draft")

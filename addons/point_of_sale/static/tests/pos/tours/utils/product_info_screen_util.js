odoo/odoo

[IMP] point_of_sale: snooze products in POS
The PR will add an extra availability section on the product info popup
which shows whether a product is currently available. From that section
the product can then be 'snoozed', which will make it unavailable for a
specified period of time. (1, 2, 4 hours, or for the entire session).

When the product is unavailable there's a countdown timer on the popup
showing when the product will be available again.

Products which are 'snoozed' still show up on the product screen, but
grayed out. The effect is purely cosmetic, as they can still be added to
new orders.

Task-[5170696](https://www.odoo.com/odoo/project/1737/tasks/5170696)

Previous discussion-[#232625](https://github.com/odoo/odoo/pull/232625)

addons/point_of_sale/static/tests/pos/tours/utils/product_info_screen_util.js
@@ -1,0 +1,33 @@
+export function productIsAvailable() {
+    return {
+        trigger: ".section-availability .btn.btn-secondary",
+        content: "Verify that the product is available",
+    };
+}
+export function productIsSnoozed() {
+    return {
+        trigger: ".section-availability .btn.btn-warning",
+        content: "Verify that the product is not available anymore",
+    };
+}
+export function clickSnoozeButton() {
+    return {
+        trigger: ".section-availability .btn",
+        content: "Click the snooze button",
+        run: "click",
+    };
+}
+export function clickAvailabilitySwitch() {
+    return {
+        trigger: ".section-availability .form-switch input[type='checkbox']:not(:checked)",
+        content: "Click the snooze button",
+        run: "click",
+    };
+}
+export function clickSnoozeDuration(button) {
+    return {
+        trigger: `.btn-group .row label:contains(${button})`,
+        content: `Click the ${button} button`,
+        run: "click",
+    };
+}

dzdzdzdz1010/OdooStandard

addons/point_of_sale/static/tests/pos/tours/utils/product_info_screen_util.js
@@ -1,0 +1,33 @@
+export function productIsAvailable() {
+    return {
+        trigger: ".section-inventory .btn.btn-secondary",
+        content: "Verify that the product is available",
+    };
+}
+export function productIsSnoozed() {
+    return {
+        trigger: ".section-inventory .btn.btn-warning",
+        content: "Verify that the product is not available anymore",
+    };
+}
+export function clickSnoozeButton() {
+    return {
+        trigger: ".section-inventory .btn",
+        content: "Click the snooze button",
+        run: "click",
+    };
+}
+export function clickAvailabilitySwitch() {
+    return {
+        trigger: ".section-inventory .form-switch input[type='checkbox']:not(:checked)",
+        content: "Click the snooze button",
+        run: "click",
+    };
+}
+export function clickSnoozeDuration(button) {
+    return {
+        trigger: `.btn-group .row label:contains(${button})`,
+        content: `Click the ${button} button`,
+        run: "click",
+    };
+}
3006d102100ea812d38dfd49bf7496482e30d4cc
Mongy (abmn) <abmn@odoo.com>
2026-02-02 16:05:53 +0100

    [IMP] *: Month Name Using Incorrect Language
    Month name is using the locale language instead of the env language
    
    Get month name in the env language
    
    task-5902364

--- addons/l10n_mt_pos/wizards/compliance_letter.py
+++ addons/l10n_mt_pos/wizards/compliance_letter.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, release, _
+from odoo import models, fields, release, tools, _
 from datetime import datetime
 from odoo.exceptions import UserError
 
@@ -26,7 +26,7 @@ class ComplianceLetter(models.TransientModel):
         date_obj = datetime.strptime(str(fields.Date.today()), '%Y-%m-%d')
         day = date_obj.day
         day_suffix = 'th' if 11 <= day <= 13 else {1: 'st', 2: 'nd', 3: 'rd'}.get(day % 10, 'th')
-        formatted_date = date_obj.strftime(f"%B {day}{day_suffix}, %Y")
+        formatted_date = f"{tools.format_date(self.env, date_obj, date_format='MMMM')} {day}{day_suffix}, {date_obj.year}"
         return formatted_date
 
     def _get_odoo_version(self):

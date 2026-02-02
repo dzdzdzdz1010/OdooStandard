6df9406fde7c16ebefed1bf43302e60449aba3ef
rhe-odoo <rhe@odoo.com>
2026-02-02 13:55:26 +0100

    [FIX] pos_restaurant: prevent double-click on release table button Uses useTrackedAsync to disable the button during the async operation. Navigates to FloorScreen before deleting to avoid re-render errors.
    task-id: 5859460

--- addons/pos_restaurant/static/src/app/screens/product_screen/order_summary/order_summary.js
+++ addons/pos_restaurant/static/src/app/screens/product_screen/order_summary/order_summary.js
@@ -1,7 +1,12 @@
 import { patch } from "@web/core/utils/patch";
 import { OrderSummary } from "@point_of_sale/app/screens/product_screen/order_summary/order_summary";
+import { useTrackedAsync } from "@point_of_sale/app/hooks/hooks";
 
 patch(OrderSummary.prototype, {
+    setup() {
+        super.setup(...arguments);
+        this.doUnbookTable = useTrackedAsync(() => this._unbookTable());
+    },
     bookTable() {
         this.pos.getOrder().setBooked(true);
         this.pos.navigate("FloorScreen");
@@ -32,8 +37,9 @@ patch(OrderSummary.prototype, {
 
         return result;
     },
-    async unbookTable() {
+    async _unbookTable() {
         const order = this.pos.getOrder();
+        this.pos.showDefault();
         await this.pos.deleteOrders([order]);
     },
     showUnbookButton() {

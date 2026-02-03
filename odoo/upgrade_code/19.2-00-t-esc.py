c2d3c87da61a172ab09c217eaf70c8aa190fc2cc
Jean Schoenlaub <jesc@odoo.com>
2026-02-03 14:50:07 +0100

    [REF] *: replace t-esc by t-out in preparation for the OWL3 migration

---
+++ odoo/upgrade_code/19.2-00-t-esc.py
@@ -0,0 +1,36 @@
+# Replaces the t-esc directive by t-out in preparation for the OWL3 migration
+
+import re
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+def upgrade(file_manager):
+    files = [file for file in file_manager if file.path.suffix == '.xml']
+    if not files:
+        return
+
+    reg_t_esc = re.compile(r"""\bt-esc=""")
+    reg_att_t_esc = re.compile(r'<attribute\s+name=["\']t-esc["\']\s*>')
+
+    for fileno, file in enumerate(files, start=1):
+        try:
+            content = file.path.read_text(encoding="utf-8")
+        except UnicodeDecodeError as e:
+            # For file enterprise/l10n_cl_edi_factoring/template/aec_template.xml
+            _logger.warning("upgrade_code: skipping non-utf8 file %s (%s)", file.path, e)
+
+        if content.startswith(("// skip_owl_ref", "# skip_owl_ref", "<!-- skip_owl_ref -->")):
+            _logger.info("Skipping file %s due to presence of skip tag", file.path)
+            continue
+
+        if "t-esc" not in content:
+            continue
+
+        # update
+        content = reg_t_esc.sub(r't-out=', content)
+        content = reg_att_t_esc.sub(r't-out=', content)
+
+        file.content = content
+        file_manager.print_progress(fileno, len(files))

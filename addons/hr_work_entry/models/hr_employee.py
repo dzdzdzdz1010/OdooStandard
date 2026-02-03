c696d29e1167bfcf5f8672b16f1127256258cf8f
romc-odoo <romc@odoo.com>
2026-02-03 10:46:19 +0100

    [FIX] style

--- addons/hr_work_entry/models/hr_employee.py
+++ addons/hr_work_entry/models/hr_employee.py
@@ -1,7 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, _
-from odoo.tools import SQL
+from odoo import models, fields
 
 
 class HrEmployee(models.Model):

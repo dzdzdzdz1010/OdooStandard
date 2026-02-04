7ac883662e9fc21a2c21f626fd70cfe46800ea44
lkerroum <leker@odoo.com>
2026-02-04 16:32:16 +0100

    [FIX]

--- addons/payment/models/res_company.py
+++ addons/payment/models/res_company.py
@@ -15,6 +15,7 @@ class ResCompany(models.Model):
         ],
         compute='_compute_onboarding_payment_module',
     )
+    # Labels to be desplayed on the payment buttons, defined per each company
     pay_now_label = fields.Char(
         string="Pay Now Label",
         help="The label to be displayed on the payment button for 'Pay Now' methods.",

646e412055ffa74be423b44d43200d8e04b7984f
Haroune Hassine (Hahas) <hahas@odoo.com>
2026-02-04 16:25:28 +0100

    [IMP] mass_mailing: add email templates management
    This commit adds  email templates management to the `mass_mailing`
    module. This allows the creation of email templates, that can be
    reused and customized by the user for fututre use, to avoid the
    creation of the full email every time for each target.

---
+++ addons/mass_mailing/models/mailing_templates.py
@@ -0,0 +1,17 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class MailingTempplates(models.Model):
+    """Mailing templates are customizable, ready to use, templates that speeds up email writing."""
+    _name = 'mailing.templates'
+    _description = 'Mailing Templates'
+    _order = "name ASC"
+
+    name = fields.Char('Mailing Template', required=True)
+    description = fields.Char('Description')
+    user_id = fields.Many2one(
+        'res.users', string='Creator',
+        tracking=True,
+        default=lambda self: self.env.user)
